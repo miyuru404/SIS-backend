50331c69404a7119650b2d308eee7af2ff772b4e
miyuru bashitha <159784923+miyuru404@users.noreply.github.com>
2025-08-13 21:41:11 +0530

    Update StudentController.java

--- src/main/java/com/demo/demospring/StudentController.java
+++ src/main/java/com/demo/demospring/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://sis-student-information-system.vercel.app/")
 @RestController
 @RequestMapping("/students")
 public class StudentController {
